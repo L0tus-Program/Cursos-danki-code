a97b7d3fc3fe38782d8aec6b4466f064bfbcdd9d
Felipe Gomes <felipelesgomes.1@gmail.com>
2023-11-01 14:28:22 -0300

    up

---
+++ variaveis e constantes/arrays.js
@@ -0,0 +1,12 @@
+let pessoas = ['Joao', 'Felipe', 'Vitor'];
+
+var nomes = new Array('Lucas','Rodolfo');
+
+console.log(pessoas[0]+"\n"+pessoas[1])
+
+pessoas.push('Testando')
+console.log(pessoas)
+
+
+
+//console.log(nomes)
